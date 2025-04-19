93c627236544ff65c343ec5a4651060eae8564dc
WonderWaffle4 <zhmelev.g@gmail.com>
2025-04-20 01:14:04 +0300

    right section design upddate. New design draft for file_information

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,10 +1,10 @@
-// Retractable profile window
-profile_container = document.querySelector(".profile-container");
-profile_small = document.querySelector(".profile-container").querySelector(".profile");
-profile_small.addEventListener("click", function() {
-    profile_container.querySelector(".profile-retractable").classList.toggle("active");
-    profile_small.classList.toggle("border");
-});
+// // Retractable profile window
+// profile_container = document.querySelector(".profile-container");
+// profile_small = document.querySelector(".profile-container").querySelector(".profile");
+// profile_small.addEventListener("click", function() {
+//     profile_container.querySelector(".profile-retractable").classList.toggle("active");
+//     profile_small.classList.toggle("border");
+// });
 
 // Repa Clips
 let isMuted = true;
