e2876f6cafff90b6f4c797a4bb9acf24fe4e7277
WonderWaffle4 <zhmelev.g@gmail.com>
2025-04-19 16:45:06 +0300

    login and registration fixes. Flash message fix

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,5 +1,4 @@
 profile_container = document.querySelector(".profile-container");
 profile_container.addEventListener("click", function() {
-    console.log("aojsdajosd");
     profile_container.querySelector(".profile-retractable").classList.toggle("active");
 });
