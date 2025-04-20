94a96fbd3078295d4fa54e3902810be3bfdfaa11
WonderWaffle4 <zhmelev.g@gmail.com>
2025-04-20 14:57:14 +0300

    image warp js script

--- backend/routes/algorithm_routes.py
+++ backend/routes/algorithm_routes.py
@@ -11,9 +11,9 @@ from ml.find_contour import find_page_contour
 @application.route("/upgrade_image/")#<string:filename>", methods=["GET"])
 def upgrade_image(filename: str = "frontend/static/paper0.jpg"):
     image = cv2.imread(filename)
-    points = list(map(lambda l: {"x": int(l[0]), "y": int(l[1]), "element": None}, map(list, list(find_page_contour(image)))))
+    points = list(map(lambda l: {"x": int(l[0]), "y": int(l[1])}, map(list, list(find_page_contour(image)))))
     print(points)
-    return render_template("upgrade_image.html", points=points, filename=filename)
+    return render_template("upgrade_image.html", points=points, filename=filename.split("/static/")[1], image_shape=tuple(image.shape[1::-1]))
 
 
 @application.route("/process_pins", methods=["POST"])
